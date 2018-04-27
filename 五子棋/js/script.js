620ce8d8a588b1ca7cd2aceece84e78d00308e24
DcySc <congyingdu00@gmail.com>
2018-04-27 21:32:02 +0800

    五子棋
    五子棋简单AI

--- 五子棋/js/script.js
+++ 五子棋/js/script.js
@@ -1,13 +1,68 @@
 var isBlack = true;
+var over = false;
 var chess = document.getElementById("chess");
 var context = chess.getContext('2d');
 var chessBoard = [];
+var wins = [];//赢法数组
+var count = 0;//赢法的数量
+var myWin = [];
+var computerWin = [];
 //初始化棋盘数组
 for(var i=0;i<15;i++){
     chessBoard[i] = [];
     for(var j=0;j<15;j++){
         chessBoard[i][j] = 0;
     }
+}
+//初始化赢法数组
+for(var i=0;i<15;i++){
+    wins[i] = [];
+    for(var j=0;j<15;j++){
+        wins[i][j] = [];
+    }
+}
+//横向五连子
+for(var i=0;i<15;i++){
+    for(var j=0;j<11;j++){
+        for(var k=0;k<5;k++){
+            wins[i][j+k][count] = true;
+        }
+        count++;
+    }
+    
+}
+
+//纵向五连子
+for(var i=0;i<15;i++){
+    for(var j=0;j<11;j++){
+        for(var k=0;k<5;k++){
+            wins[j+k][i][count] = true;
+        }
+        count++;
+    }
+}
+//正斜向五连子
+for(var i=0;i<11;i++){
+    for(var j=0;j<11;j++){
+        for(var k=0;k<5;k++){
+            wins[i+k][j+k][count] = true;
+        }
+        count++;
+    }
+}
+//反斜向五连子
+for(var i=0;i<11;i++){
+    for(var j=14;j>3;j--){
+        for(var k=0;k<5;k++){
+            wins[i+k][j-k][count] = true;
+        }
+        count++;
+    }
+}
+
+for(var i=0;i<count;i++){
+    myWin[i] = 0;
+    computerWin[i] = 0;
 }
 
 context.strokeStyle = "#BFBFBF";
@@ -63,20 +118,111 @@ function oneStep(x,y,isBlack){
  * @param {*} e 
  */
 function fall(e){
+    if(over) return;
+    if(!isBlack) return;
     
     var x = e.offsetX;
     var y = e.offsetY;
     var i = x / 30 | 0;
     var j = y / 30 | 0; 
-    console.log(x,y,i,j)
+
     if(!chessBoard[i][j]){
-        oneStep(i,j,isBlack);
-        if(isBlack){
-            chessBoard[i][j] = 1;
-        }else{
-            chessBoard[i][j] = 2;
+        oneStep(i,j,isBlack);       
+        chessBoard[i][j] = 1;
+
+        //每次落子判断胜负
+        for(var k=0;k<count;k++){
+            if(wins[i][j][k]){
+                myWin[k]++;
+                computerWin[k] = 6;
+                if(myWin[k] === 5){
+                    alert("你赢了")
+                    over = true;  
+                }
+            }
+        }   
+        if(!over){
+            isBlack = !isBlack;
+            computerAI();
         }
-        console.log(chessBoard);
+    }
+}
+
+function computerAI(){
+    var computerScore = [];
+    var myScore = [];
+    var max = 0;
+    var x=0,y=0;
+    for(var i=0;i<15;i++){
+        computerScore[i] = [];
+        myScore[i] = [];
+        for(var j=0;j<15;j++){
+            computerScore[i][j] = 0;
+            myScore[i][j] = 0;
+        }
+    }
+    for(var i=0;i<15;i++){
+        for(var j=0;j<15;j++){
+            if(chessBoard[i][j] === 0){
+                for(var k=0;k<count;k++){
+                    if(wins[i][j][k]){
+                        if(myWin[k] === 1){
+                            myScore[i][j] += 200;
+                        }else if(myWin[k] === 2){
+                            myScore[i][j] += 400;
+                        }else if(myWin[k] === 3){
+                            myScore[i][j] += 2000;
+                        }else if(myWin[k] === 4){
+                            myScore[i][j] += 10000;
+                        }
+                        if(computerWin[k] === 1){
+                            computerScore[i][j] += 399;
+                        }else if(myWin[k] === 2){
+                            computerScore[i][j] += 1999;
+                        }else if(myWin[k] === 3){
+                            computerScore[i][j] += 9999;
+                        }else if(myWin[k] === 4){
+                            computerScore[i][j] += 50000;
+                        }
+                    }
+                }
+                console.log(computerScore[i][j],myScore[i][j])
+                if(myScore[i][j] > max){
+                    max = myScore[i][j];
+                    x = i;
+                    y = j;
+                }else if(myScore[i][j] === max){
+                    if(computerScore[i][j] > computerScore[x][y]){
+                        x = i;
+                        y = j;
+                    }
+                }
+                if(computerScore[i][j] > max){
+                    max = computerScore[i][j];
+                    x = i;
+                    y = j;
+                }else if(computerScore[i][j] === max){
+                    if(myScore[i][j] > myScore[x][y]){
+                        x = i;
+                        y = j;
+                    }
+                }
+            }
+        }
+    }
+    oneStep(x,y,false);
+    chessBoard[x][y] = 2;
+    for(var k=0;k<count;k++){
+        if(wins[x][y][k]){
+            computerWin[k]++;
+            myWin[k] = 6;
+            if(computerWin[k] === 5){
+                alert("电脑赢了")
+                over = true;  
+            }
+        }
+    }   
+    if(!over){
         isBlack = !isBlack;
     }
 }
